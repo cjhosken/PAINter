77da5f9f6a658ef49ec5b5025bd1b0431375fc56
Christopher Hosken <hoskenchristopher@gmail.com>
2023-11-15 10:29:21 +0000

    SDL implementation

--- Makefile
+++ Makefile
@@ -7,8 +7,8 @@ DATE=15.11.2023
 
 CC=gcc
 
-painter: main.c
-	$(CC) -o $(NAME) main.c
+painter:
+	$(CC) -lSDL2 -o $(NAME) main.c
 	echo "PAINter has been compiled!"
 
 # Copyright © 2023 Christopher Hosken
--- main.c
+++ main.c
@@ -1,7 +1,32 @@
-#include <SDL.h>
+#include "include/SDL2/SDL.h"
 
+const int APP_WIDTH = 1280;
+const int APP_HEIGHT = 720;
+
+//https://gigi.nullneuron.net/gigilabs/sdl2-pixel-drawing/
 int main(int argc, char** argv) {
+
+    char quit = 0;
+    SDL_Event event;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("PAINter", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, APP_WIDTH, APP_HEIGHT, 0);
+
+    while (!quit) {
+        SDL_WaitEvent(&event);
+
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                quit=1;
+                break;
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
+//end
 
 // Copyright © 2023 Christopher Hosken
